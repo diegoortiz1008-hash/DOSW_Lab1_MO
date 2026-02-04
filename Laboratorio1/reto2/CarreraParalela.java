71084764c9e4a52e34aa2019c92231206c29452b
diegoortiz1008-hash <diegoortiz1008@gmail.com>
2026-02-03 20:58:21 -0500

    intento de fallo

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -1,15 +1,25 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class CarreraParalela{
 	public CarreraParalela(){
-		System.out.println("Carrera de buces")
+		System.out.println("Carrera de buces");
 	}
-	public int numeroMasGrande(ArrayList<Integer> numeros){
+	public int numeroMasGrande(List<Integer> numeros){
 		return numeros.stream().max(Integer::compare).get();
-	{
-	public int numeroMasPequeño(ArrayList<Integer> numeros){
-	       return numero.stream().min(Integer::compare).get();
 	}
-.	
+	public int numeroMasPequeño(List<Integer> numeros){
+	       return numeros.stream().min(Integer::compare).get();
+	}
+
+    public List<Integer> resultado(List<Integer> buses) {
+
+        return Stream.of(
+                numeroMasGrande(buses),
+                numeroMasPequeño(buses),
+                buses.size()
+        ).toList();
+    }
 }
