7232724cba9dcf412ebf093d5d7e0e9462a08d45
diegoortiz1008-hash <diegoortiz1008@gmail.com>
2026-02-04 20:54:19 -0500

    Implementación funcion reverse y Función combinada

---
+++ Laboratorio1/reto1/WelcomeMessage.java
@@ -0,0 +1,12 @@
+public class WelcomeMessage {
+
+    private String message;
+
+    public WelcomeMessage(String newMessage) {
+        message = newMessage;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
--- Laboratorio1/reto3/StudentAThirdChange.java
+++ Laboratorio1/reto3/StudentAThirdChange.java
@@ -1,4 +1,5 @@
 
+import java.util.function.Function;
 import java.util.stream.IntStream;
 
 public class StudentAThirdChange {
@@ -7,4 +8,31 @@ public class StudentAThirdChange {
         IntStream.range(0, 2).forEach(n -> sb.append(" ").append(words));
         return sb.toString();
     }
+
+    public String ecoBuffer(String mensaje) {
+
+            StringBuffer buffer = new StringBuffer(mensaje); 
+            buffer.reverse();
+            return buffer.toString();
+    }
+
+    public String finalReto3(String mensaje){
+        String mensajeFinal = wordRepet(mensaje);
+        mensajeFinal = ecoBuffer(mensajeFinal);
+        return mensajeFinal;
+    }
+
+    public static void main(String[] args) {
+        StudentAThirdChange obj = new StudentAThirdChange();
+
+        String input = "Hola Mundo";
+        String obtenido1 = obj.wordRepet(input);
+        System.out.println(obtenido1);
+        String obtenido2 = obj.ecoBuffer(input);
+        System.out.println(obtenido2);
+        String obtenido3  = obj.finalReto3(input);
+        System.out.println(obtenido3);
+
+    }
+
 }
