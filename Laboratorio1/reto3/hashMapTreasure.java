cdb1d218cdeaa8d8952343923feaeee309678750
wombitoCol <julio.mayorquin@mail.escuelaing.edu.co>
2026-02-05 20:50:37 -0500

    hashMap union Hashtable

--- Laboratorio1/reto3/hashMapTreasure.java
+++ Laboratorio1/reto3/hashMapTreasure.java
@@ -1,6 +1,7 @@
 package reto3;
 
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -12,5 +13,10 @@ class hashMapTreasure {
                 Map.Entry::getValue,
                 (first, second) -> second,
                 HashMap::new));
+    }
+    public Hashtable<String, Integer> unionMapTable(HashMap<String, Integer> a, Hashtable<String, Integer> b) {
+        Hashtable<String, Integer> result = new Hashtable<>(b);
+        a.forEach((key, value) -> result.put(key, value));
+        return result;
     }
 }
